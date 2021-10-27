ebc4f809b0d49bda0bdd78ea7f879c4f0f372b1b
Yusuf <yusufgamer222@gmail.com>
2021-10-27 16:40:01 +0100

    fixed

--- src/main/java/github/io/yusuf/core/bot/slash_command/CoreSlashCommandHandler.java
+++ src/main/java/github/io/yusuf/core/bot/slash_command/CoreSlashCommandHandler.java
@@ -2,16 +2,20 @@ package github.io.yusuf.core.bot.slash_command;
 
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class CoreSlashCommandHandler extends ListenerAdapter {
+    SlashCommandEvent event;
     private final Map<String, Command> commands = new HashMap<>();
+    CommandListUpdateAction commandsData = event.getJDA().updateCommands();
 
 
     public void addCommand(Command command){
         commands.put(command.getName(), command);
+        commandsData.addCommands(command.getCommandData());
     }
 
     @Override
