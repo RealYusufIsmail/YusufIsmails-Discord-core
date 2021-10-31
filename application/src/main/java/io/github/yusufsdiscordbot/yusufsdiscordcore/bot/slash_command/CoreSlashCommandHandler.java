8c71b2206eb4078de40d4336d6b5e9b9b5e45b76
Yusuf <yusufgamer222@gmail.com>
2021-10-31 19:56:04 +0000

    update javaDoc

--- application/src/main/java/io/github/yusufsdiscordbot/yusufsdiscordcore/bot/slash_command/CoreSlashCommandHandler.java
+++ application/src/main/java/io/github/yusufsdiscordbot/yusufsdiscordcore/bot/slash_command/CoreSlashCommandHandler.java
@@ -578,8 +578,8 @@ public class CoreSlashCommandHandler extends ListenerAdapter {
     public CommandListUpdateAction guildCommandsData;
 
     /**
-     * For an example please see
-     * <a href="https://github.com/YusufsDiscordbot/Yusuf-s-Moderation-Bot/blob/JDA-Development/application/src/main/java/net/yusuf/bot/CommandHandler.java">example</a>
+     * For an example please see <a href=
+     * "https://github.com/YusufsDiscordbot/Yusuf-s-Moderation-Bot/blob/JDA-Development/application/src/main/java/net/yusuf/bot/CommandHandler.java">example</a>
      */
     public CoreSlashCommandHandler(JDA jda, Guild guild) {
         globalCommandsData = jda.updateCommands();
