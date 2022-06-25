bb6845c05c602bf8a03731135ee63e0246648c70
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-06-25 20:19:33 +0100

    A lot of changes

---
+++ jda5.basic/src/main/java/io/github/yusufsdiscordbot/yusufsdiscordcore/bot/extension/SlashCommandExtender.java
@@ -0,0 +1,35 @@
+package io.github.yusufsdiscordbot.yusufsdiscordcore.bot.extension;
+
+import io.github.yusufsdiscordbot.yusufsdiscordcore.bot.builder.slash.SlashCommand;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+
+public abstract class SlashCommandExtender {
+
+    // This method is called when the command is executed.
+    public abstract void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event);
+
+    public void onButtonClick(ButtonInteractionEvent event) {
+
+    }
+
+    public void onModalInteraction(ModalInteractionEvent event) {
+
+    }
+
+    public void onCommandAutoComplete(@Nonnull CommandAutoCompleteInteractionEvent event) {
+
+    }
+
+    public void onSelectMenu(@Nonnull SelectMenuInteractionEvent event) {
+
+    }
+
+    public abstract SlashCommand build();
+}
---
+++ jda5.basic/src/main/java/io/github/yusufsdiscordbot/yusufsdiscordcore/bot/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * All the command handling classes are located here
+ */
+package io.github.yusufsdiscordbot.yusufsdiscordcore.bot;
